47b0775c3b14bd6f7c54288e82b374334c2e0034
Olga <o.pogibelnaya@g.nsu.ru>
2023-11-02 00:19:14 +0700

    создание черновика и его отправка

---
+++ test_gmail/create_message.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+
+import base64
+from email.message import EmailMessage
+
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
+
+
+def gmail_create_draft():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    try:
+        # create gmail api client
+        service = build('gmail', 'v1', credentials=creds)
+
+        message = EmailMessage()
+
+        message.set_content('some content')
+
+        message['To'] = 'gduser2@workspacesamples.dev'
+        message['From'] = 'gduser2@workspacesamples.dev'
+        message['Subject'] = 'Hello from Python'
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {
+            'message': {
+                'raw': encoded_message
+            }
+        }
+        # pylint: disable=E1101
+        draft = service.users().drafts().create(userId="me",
+                                                body=create_message).execute()
+
+        #service.users().drafts().send(userId="me", body=draft).execute()
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        draft = None
+
+    return draft
+
+
+if __name__ == '__main__':
+    gmail_create_draft()
