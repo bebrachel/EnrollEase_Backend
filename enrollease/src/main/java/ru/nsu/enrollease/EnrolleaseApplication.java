6f6e6763fe37b3950cd8e4b64805c1f726d90db4
Nikolay <58514394+nocarend@users.noreply.github.com>
2023-11-18 22:57:07 +0700

    spring init

---
+++ enrollease/src/main/java/ru/nsu/enrollease/EnrolleaseApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.enrollease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrolleaseApplication.class, args);
+	}
+
+}
