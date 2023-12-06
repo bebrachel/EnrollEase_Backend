a98a7f2017a7e88f1948d1b6627f619a8522755d
Nikolay <58514394+nocarend@users.noreply.github.com>
2023-12-06 14:01:46 +0700

    script relative path

--- scripts/excel_to_mongo/script.py
+++ scripts/excel_to_mongo/script.py
@@ -1,6 +1,5 @@
 def export_from_excel(filename):
     import pandas as pd
-    import os
     from pymongo import MongoClient
     excel_file = filename
     data_frame = pd.read_excel(excel_file)
@@ -23,7 +22,13 @@ def export_from_excel(filename):
             if str(document[i]) == 'nan' or i == primary_key:
                 c_d.pop(i)
         doc = {primary_key: document[primary_key], 'data': c_d}
-        collection.update_one({primary_key: document[primary_key]}, {"$set": doc}, upsert=True)
+        result = collection.update_one({primary_key: document[primary_key]}, {"$set": doc}, upsert=True).raw_result
+        if not result.get('updatedExisting'):
+            mail = document['Email']
+            name = document['ФИО']
+            import os
+            os.system(
+                f'cd ../google_drive_creation && python script.py "Индивидуальные достижения {name}" "{mail}"')
 
 
 if __name__ == '__main__':
