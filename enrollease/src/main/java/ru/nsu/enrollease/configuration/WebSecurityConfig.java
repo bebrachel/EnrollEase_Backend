39ee55b01f64a86ebe7b9a9ee84293eef95953a6
Nikolay <58514394+nocarend@users.noreply.github.com>
2023-12-07 17:52:56 +0700

    role giving

--- enrollease/src/main/java/ru/nsu/enrollease/configuration/WebSecurityConfig.java
+++ enrollease/src/main/java/ru/nsu/enrollease/configuration/WebSecurityConfig.java
@@ -3,34 +3,35 @@ package ru.nsu.enrollease.configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.Collection;
-import java.util.Collections;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.nsu.enrollease.service.ColleagueService;
+import ru.nsu.enrollease.service.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    @Autowired
-    private ColleagueService colleagueService;
+    private final ColleagueService colleagueService;
+    private final MyUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,6 +41,7 @@ public class WebSecurityConfig {
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/swagger-ui/**").permitAll();
                 auth.requestMatchers("/api-docs/**").permitAll();
+                auth.requestMatchers("/admin/**").hasAuthority("HEAD_OF_COMMISSION");
                 auth.requestMatchers("/**").authenticated();
             })
             .csrf(AbstractHttpConfigurer::disable)
@@ -55,11 +57,10 @@ public class WebSecurityConfig {
             @Override
             public Collection<GrantedAuthority> convert(Jwt jwt) {
                 String email = (String) jwt.getClaims().get("email");
-                System.out.println(email);
                 // Проверяем, существует ли email в базе данных
                 if (colleagueService.isAllowed(email)) {
                     // роли делать
-                    return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                    return (Collection<GrantedAuthority>) userDetailsService.loadUserByUsername(email).getAuthorities();
                 }
                 throw new RuntimeException("Email not found in database");
             }
@@ -84,4 +85,21 @@ public class WebSecurityConfig {
             }
         };
     }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "HEAD_OF_COMMISSION > DEFAULT_COLLEAGUE";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
+
+    @Bean
+    public DefaultWebSecurityExpressionHandler customWebSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler =
+            new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
+    }
+
 }
