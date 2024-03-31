91fd5c8d2427d4a474319ddf7ad96b4a07e0a925
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-03-31 23:57:22 +0700

    java import, write close/open, sertificates gen

---
+++ backend-java/src/main/java/ru/nsu/enrollease/dto/request/applicant_portfolio/ChangePermissionsRequest.java
@@ -0,0 +1,4 @@
+package ru.nsu.enrollease.dto.request.applicant_portfolio;
+
+public record ChangePermissionsRequest(boolean write) {
+}
---
+++ backend-java/src/main/java/ru/nsu/enrollease/dto/response/applicant_portfolio/NumberOfCertificatesResponse.java
@@ -0,0 +1,4 @@
+package ru.nsu.enrollease.dto.response.applicant_portfolio;
+
+public record NumberOfCertificatesResponse(long winners, long finalists) {
+}
